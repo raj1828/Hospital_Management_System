5b946d5b0160c0f3ae74f1447f2f535e3a398102
sakshi2125 <sakshibhopatrao2125@gmail.com>
2021-09-07 19:00:38 +0530

    Added review operation

---
+++ db/dr_review.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 07, 2021 at 03:20 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dr_review`
+--
+
+CREATE TABLE `dr_review` (
+  `Name` text NOT NULL,
+  `ID` int(11) NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL,
+  `Symptoms` text NOT NULL,
+  `Reports` text NOT NULL,
+  `Prescription` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ review.php
@@ -0,0 +1,59 @@
+<?php
+
+	$p_name=$_POST['name'];
+	$p_id=$_POST['pid'];
+	$p_date=$_POST['date'];
+	$p_time=$_POST['time'];
+	$p_symptoms=$_POST['symptoms'];
+	$p_reports=$_POST['reports'];
+	$p_prescriptions=$_POST['prescription'];
+
+
+	$servername="localhost";
+	$username="root";
+	$password="";
+	$dbname="hms";
+
+	$conn=new mysqli($servername,$username,$password,$dbname);
+
+	if(isset($_POST['Review']))
+	{
+		if (!$conn)
+		{
+			die('Could not connect'.mysqli_connect_error());
+
+		}
+		$sql= "insert into dr_review(Name,ID,Date,Time,Symptoms,Reports,Prescription) values ('$p_name', '$p_id', '$p_date', '$p_time', '$p_symptoms', '$p_reports', '$p_prescriptions')";
+		if($conn->query($sql) == true )
+		{
+			echo "Data has been added successfully!!";
+		}
+		else
+		{
+			echo "Error!".$conn->error;
+		}
+		$conn->close();
+	}
+
+	// if(isset($_POST['ViewData']))
+	// {
+	// 	if (!$conn)
+	// 	 {
+	// 		die('Could not connect'.mysqli_connect_error());
+	// 	}
+	// 	$sql="select * from customer";
+	// 	$retval=mysqli_query($conn,$sql);
+	// 	if($row=mysqli_fetch_array($retval))
+	// 	{
+	// 		echo "<br>Customer Name : ".$row["customer_name"]."<br>Account Number : ".$row["account_no"]."<br>Customer Age : ".$row["customer_age"]. "<br>Ammount retrieved : ".$row["customer_salary"];
+	// 	}
+	// 	else
+	// 	{
+	// 		echo "Error!".$conn->error;
+	// 	}
+	// 	$conn->close();
+		
+	// }
+
+
+?>
